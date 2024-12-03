b49e3f92f7096d51232b2aa96789179f9d127629
Ruthvik m r <ruthvikmr417@gmail.com>
2024-12-04 00:36:02 +0530

    2nd Patch to main
    Patch Reason :  React Hook useEffect has a missing dependency: 'getNewsList'. Either include it or remove the dependency array
    Fix : Fix is done by adding useCallBack and proper use of useEffect

--- src/components/NewsAI/ListNews.js
+++ src/components/NewsAI/ListNews.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from "react";
-import {  DateFormatter } from '../../Util/lib'
+import React, { useCallback, useEffect, useState } from "react";
+import { DateFormatter } from '../../Util/lib'
 import { config } from '../../Util/config'
 import { Col, Image, Row } from "react-bootstrap";
 import Placeholder from 'react-bootstrap/Placeholder';
@@ -9,19 +9,10 @@ import Placeholder from 'react-bootstrap/Placeholder';
 export function ListNews() {
     const [page, setPage] = useState(1);
     const [newsData, setNewsData] = useState([]);
-    const [loader,setLoader]= useState(false);
- 
-
-
-    useEffect(() => {
-        setLoader(true);
-        getNewsList("");
-    }, []);
-
-
-    const getNewsList = async (nextPage = false) => {
+    const [loader, setLoader] = useState(false);
+    
+    const getNewsList = useCallback(async (nextPage = false) => {
         try {
-            console.log("nexPage", nextPage)
             const response = await fetch(
                 `${config.api_url}news?page=${nextPage ? page + 1 : 1}`
             );
@@ -30,7 +21,6 @@ export function ListNews() {
             }
             const data = await response.json();
             if (response?.status === 200) {
-                console.log(data);
                 const articles = data?.articles || [];
                 if (nextPage) {
                     setPage(page + 1);
@@ -46,32 +36,38 @@ export function ListNews() {
         } catch (error) {
             console.error(error);
         }
-    }
+    },[page]);
+
+    useEffect(() => {
+        setLoader(true);
+        getNewsList("");
+    }, [getNewsList]);
+
     return (
         <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-        {!loader && newsData?.length > 0 ? (
-          newsData.map((data, key) => (
-            <Col xs={4} md={3} className="news-icons" key={key}>
-              <Image src={data?.urlToImage} fluid /><br />
-              <DateFormatter dateString={data?.publishedAt} />
-              <h5>{data?.title}</h5>
-            </Col>
-          ))
-        ) : (
-          <Col xs={12} style={{ textAlign: "center" }}>
-            <h5>Loading...!</h5>
-            <Placeholder animation="glow">
-              <Placeholder xs={6} />
-              <Placeholder xs={4} />
-              <Placeholder xs={8} />
-              <Placeholder xs={6} />
-              <Placeholder xs={8} />
-              <Placeholder xs={6} />
-              <Placeholder xs={8} />
-            </Placeholder>
-          </Col>
-        )}
-      </Row>
-      
+            {!loader && newsData?.length > 0 ? (
+                newsData.map((data, key) => (
+                    <Col xs={4} md={3} className="news-icons" key={key}>
+                        <Image src={data?.urlToImage} fluid /><br />
+                        <DateFormatter dateString={data?.publishedAt} />
+                        <h5>{data?.title}</h5>
+                    </Col>
+                ))
+            ) : (
+                <Col xs={12} style={{ textAlign: "center" }}>
+                    <h5>Loading...!</h5>
+                    <Placeholder animation="glow">
+                        <Placeholder xs={6} />
+                        <Placeholder xs={4} />
+                        <Placeholder xs={8} />
+                        <Placeholder xs={6} />
+                        <Placeholder xs={8} />
+                        <Placeholder xs={6} />
+                        <Placeholder xs={8} />
+                    </Placeholder>
+                </Col>
+            )}
+        </Row>
+
     )
 }
