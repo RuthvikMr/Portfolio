a1aee8f2317944479a45c1c288896f7ff84a4ba1
Ruthvik m r <ruthvikmr417@gmail.com>
2025-01-23 01:05:19 +0530

    Enhancement of Guid Screen and Home Screen Font

--- src/components/Guide/Guide.js
+++ src/components/Guide/Guide.js
@@ -29,14 +29,15 @@ const IFrameComponent = (props) => {
     const { t } = useTranslation();
     const [isMobile, setIsMobile] = useState(false);
     const [menuItems, setMenuItems] = useState();
+    const [selectedItem, setSelectedItem] = useState('0');
 
     const items = () => {
-        const menuItm = iframe.map((item,key)=>{
-            return getItem(item.label,key,item.url,<FaGithub/>)
+        const menuItm = iframe.map((item, key) => {
+            return getItem(item.label, key, item.url, <FaGithub />)
         });
         setMenuItems(menuItm);
     }
-    
+
 
 
     // Detect screen size changes
@@ -78,6 +79,7 @@ const IFrameComponent = (props) => {
         try {
             const selectedItem = menuItems.find((item) => item.key === Number($event.key));
             if (selectedItem) {
+                setSelectedItem($event.key);
                 setTimeout(() => {
                     loadGuidUrl(selectedItem?.url);
                 }, 1000);
@@ -90,104 +92,125 @@ const IFrameComponent = (props) => {
 
     return (
         <>
-            {!loader ? (
-                <>
-                    <div className="guideSection">
-                        <div className="d-flex justify-content-between h-100">
-                            {/* Mobile View  */}
-                            {isMobile ? (
-                               <>
-                               <div className="guideSection">
-                                 <div className="d-flex flex-column h-100">
-                                   {/* Top Chips/Menu */}
-                                   <div className="top-menu-container">
-                                   <div className="chips-container">
-                                         {menuItems.map((item) => (
-                                           <Button
-                                             key={item.key}
-                                             type="default"
-                                             icon={item.icon}
-                                             onClick={onTabChange}
-                                             style={{ margin: "5px" }}
-                                           >
-                                             {item.label}
-                                           </Button>
-                                         ))}
-                                       </div>
-                                   </div>
-                                   {/* Main Content: IFrame */}
-                                   <iframe
-                                     src={url}
-                                     title="Embedded Page"
-                                     className="iFramePage"
-                                   ></iframe>
-                                 </div>
-                               </div>
-                             </>
-                            ) : (
-                                <>
-                                    {/* Full Screen  */}
-                                    {/* Flex Box 1  */}
-                                    <div className="d-flex align-items-end">
-                                        <img
-                                            src={reading}
-                                            alt="Failed to load"
-                                            className="img-fluid guide-logo"
-                                            style={{ maxHeight: "100px" }}
-                                        />
-
+            <div className="guideSection">
+                <div className="d-flex justify-content-between h-100">
+                    {/* Mobile View  */}
+                    {isMobile ? (
+                        <>
+                            <div className="guideSection">
+                                <div className="d-flex flex-column h-100">
+                                    {/* Top Chips/Menu */}
+                                    <div className="top-menu-container">
+                                        <div className="chips-container">
+                                            {menuItems.map((item) => (
+                                                <Button
+                                                    key={item.key}
+                                                    type="default"
+                                                    icon={item.icon}
+                                                    onClick={() => onTabChange(item)}
+                                                    style={{ margin: "5px" }}
+                                                >
+                                                    {item.label}
+                                                </Button>
+                                            ))}
+                                        </div>
                                     </div>
+                                    {/* Main Content: IFrame */}
+                                    {!loader ? (
+                                        <iframe
+                                            src={url}
+                                            title="Embedded Page"
+                                            className="iFramePage"
+                                        ></iframe>
+                                    ) : (
+                                        loader ? (
+                                            <Col
+                                                xs={12}
+                                                style={{
+                                                    display: "flex",
+                                                    justifyContent: "center",
+                                                    alignItems: "center",
+                                                    height: "100vh", // Optional: if you want it vertically centered within the page
+                                                }}
+                                            >
+                                                <h5 className="purple">{t('messages.loading')}</h5>
+                                            </Col>
+                                        ) : (
+                                            <Col xs={12} style={{ textAlign: "center" }}>
+                                                <h5>{t('messages.noNewsAvailable')}</h5>
+                                            </Col>
+                                        )
+                                    )}
+                                </div>
+                            </div>
+                        </>
+                    ) : (
+                        <>
+                            {/* Full Screen  */}
+                            {/* Flex Box 1  */}
+                            <div className="d-flex align-items-end">
+                                <img
+                                    src={reading}
+                                    alt="Failed to load"
+                                    className="img-fluid guide-logo"
+                                    style={{ maxHeight: "100px" }}
+                                />
 
-                                    {/* Flex Box 2  */}
-                                    <iframe
-                                        src={url}
-                                        title="Embedded Page"
-                                        className="iFramePage"
-                                    ></iframe>
-
-                                    {/* Flex Box 3  */}
-                                    <div>
-                                        <Button
-                                            type="text"
-                                            className="bg-transparent text-white collapse-btn"
-                                            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-                                            onClick={() => setCollapsed(!collapsed)}
+                            </div>
+                            <div  className="iFramePage">
+                                {!loader ? (
+                                    <>
+                                        {/* Flex Box 2  */}
+                                        <iframe
+                                            src={url}
+                                            title="Embedded Page"
+                                            className="iFramePage"
+                                        ></iframe>
+                                    </>
+                                ) : (
+                                    loader ? (
+                                        <Col
+                                            xs={12}
                                             style={{
-                                                fontSize: '16px',
-                                                width: 64,
-                                                height: 64,
+                                                display: "flex",
+                                                justifyContent: "center",
+                                                alignItems: "center",
+                                                height: "100vh",
                                             }}
-                                        />
-                                        <Sider collapsed={collapsed} onCollapse={(value) => setCollapsed(value)} className="custom-sider">
-                                            <div className="demo-logo-vertical" />
-                                            <Menu className="bg-transparent custom-menu" defaultSelectedKeys={['0']} mode="inline" items={menuItems} onClick={onTabChange} />
-                                        </Sider>
-                                    </div>
-                                </>
-                            )
-                            }
-                        </div>
-                    </div>
-                </>
-            ) : (
-                loader ? (
-                    <Col
-                        xs={12}
-                        style={{
-                            display: "flex",
-                            justifyContent: "center",
-                            alignItems: "center",
-                            height: "100vh", // Optional: if you want it vertically centered within the page
-                        }}
-                    >
-                        <h5 className="purple">{t('messages.loading')}</h5>
-                    </Col>
-                ) : (
-                    <Col xs={12} style={{ textAlign: "center" }}>
-                        <h5>{t('messages.noNewsAvailable')}</h5>
-                    </Col>
-                )
-            )}
+                                        >
+                                            <h5 className="purple">{t('messages.loading')}</h5>
+                                        </Col>
+                                    ) : (
+                                        <Col xs={12} style={{ textAlign: "center" }}>
+                                            <h5>{t('messages.noNewsAvailable')}</h5>
+                                        </Col>
+                                    )
+                                )}
+                            </div>
+
+                            {/* Flex Box 3  */}
+                            <div>
+                                <Button
+                                    type="text"
+                                    className="bg-transparent text-white collapse-btn"
+                                    icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+                                    onClick={() => setCollapsed(!collapsed)}
+                                    style={{
+                                        fontSize: '16px',
+                                        width: 64,
+                                        height: 64,
+                                    }}
+                                />
+                                <Sider collapsed={collapsed} onCollapse={(value) => setCollapsed(value)} className="custom-sider">
+                                    <div className="demo-logo-vertical" />
+                                    <Menu className="bg-transparent custom-menu" defaultSelectedKeys={[selectedItem]} mode="inline" items={menuItems} onClick={onTabChange} />
+                                </Sider>
+                            </div>
+                        </>
+                    )
+                    }
+                </div>
+            </div>
         </>
     );
 };
